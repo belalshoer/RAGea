05466e5bb8aead51980a141774cec26a728d4a38
khaledelhady44 <khaledelhady44@gmail.com>
2025-10-10 18:41:52 +0400

    Add vector store implementation with FAISS and initial requirements

---
+++ src/ragea/vectorstores/base.py
@@ -0,0 +1,29 @@
+# repos/base.py
+from abc import ABC, abstractmethod
+from pathlib import Path
+from typing import List, Any
+from langchain.schema import Document
+from langchain_core.retrievers import BaseRetriever
+
+
+class VectorStore(ABC):
+    """
+    Abstract persistence boundary for a local vector store.
+    """
+
+    name: str
+    dir: Path
+
+    @abstractmethod
+    def exists(self) -> bool: ...
+
+    @abstractmethod
+    def create_from_documents(self, chunks: List[Document]) -> None: ...
+
+    @abstractmethod
+    def add_documents(self, chunks: List[Document]) -> int:
+        """Returns number of chunks added."""
+
+    @abstractmethod
+    def as_retriever(self, k: int = 5)  -> BaseRetriever:
+        """Return a retriever bound to this store."""
